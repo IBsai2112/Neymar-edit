d56e18f9d6a5456f2f51643463547c4c1d2606eb
Sainjal Kalnekar <sainjalkalnekar21@gmail.com>
2026-02-02 11:44:51 +0530

    Fixing animation bug- Fixing animation bug (skiping the initail two images) - Adding more images (Animation need more images then I thought it would)

--- src/pages/agence.jsx
+++ src/pages/agence.jsx
@@ -9,20 +9,30 @@ const agence = () => {
   const imageRef = useRef(null)
 
   const imageArr = [
-    '/public/4-ney.jpeg',
-    '/public/assets/1-Ney.jpeg',
-    '/public/assets/2-Ney.jpeg',
-    '/public/assets/3-Ney.jpeg',
-    '/public/assets/5-ney.jpeg',
-    '/public/assets/6-ney.jpeg',
-    '/public/assets/7-ney.jpeg',
-    '/public/assets/8-ney.jpg',
-    '/public/assets/9-ney.jpeg',
-    '/public/assets/10-ney.jpeg',
-    '/public/assets/11-ney.jpeg',
-    '/public/assets/12-ney.jpeg',
-    '/public/assets/13-ney.jpeg',
-  ];
+  '/assets/1-Ney.jpeg',
+  '/assets/2-Ney.jpeg',
+  '/assets/3-Ney.jpeg',
+  '/public/4-ney.jpeg',
+  '/assets/5-ney.jpeg',
+  '/assets/6-ney.jpeg',
+  '/assets/7-ney.jpeg',
+  '/assets/8-ney.jpg',
+  '/assets/9-ney.jpeg',
+  '/assets/10-ney.jpeg',
+  '/assets/11-Ney.jpeg',
+  '/assets/12-ney.jpeg',
+  '/assets/13-ney.jpeg',
+  '/assets/14Ney.jpeg',
+  '/assets/15Ney.jpeg',
+  '/assets/16Ney.jpeg',
+  '/assets/17Ney.jpeg',
+  '/assets/18Ney.jpeg',
+  '/assets/19Ney.jpeg',
+  '/assets/20Ney.jpeg',
+  '/assets/21Ney.jpeg',
+  '/assets/22Ney.jpeg',
+  '/assets/23Ney.jpeg',
+]
   
 
 
@@ -31,7 +41,7 @@ const agence = () => {
 
       scrollTrigger: {
         trigger:imageDivRef.current,
-        markers:true,
+        // markers:true,
         start:"top 21%",
         end:"top -90%",
         pin:true,
@@ -39,7 +49,7 @@ const agence = () => {
           let imagesIndex;
 
           if (elem.progress < 1){
-            imagesIndex = Math.floor(elem.progress * imageArr.length )
+            imagesIndex = Math.round(elem.progress * (imageArr.length - 2))
           } else { 
             imagesIndex = imageArr.length -1
           }
@@ -57,12 +67,12 @@ const agence = () => {
       <img ref={imageRef} className='w-full h-full object-cover' src="/public/4-ney.jpeg" alt="Neymar Jr"/>
     </div>
      <div className='relative font-[font2]'>
-      <div className='mt-[58vh]'>
-        <h1 className='text-[11vw] text-center uppercase leading-[13vw]'>Neymar da Silva <br/>
-           Santos Júnior</h1>
+      <div className='mt-[60vh]'>
+        <h1 className='text-[13vw] text-center uppercase leading-[13vw]'>Neymar&nbsp;&nbsp;da Silva&nbsp;
+           Santos &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Júnior</h1>
       </div>
       <div className='mt-15 pl-[40%]'>
-        <p className='text-3xl '>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Neymar da Silva Santos Júnior, born in 1992, is a legendary Brazilian forward recognized for his exceptional dribbling, flair, and playmaking, ranking as Brazil's top international goalscorer. He achieved fame with Santos FC before thriving at FC Barcelona (part of the 'MSN' trio) and PSG, winning numerous titles including the UEFA Champions League. </p>
+        <p className='text-3xl leading-[3.5vw]'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Neymar da Silva Santos Júnior, born in 1992, is a legendary Brazilian forward recognized for his exceptional dribbling, flair, and playmaking, ranking as Brazil's top international goalscorer. He achieved fame with Santos FC before thriving at FC Barcelona (part of the 'MSN' trio) and PSG, winning numerous titles including the UEFA Champions League. </p>
       </div>
     </div>
    </div>
