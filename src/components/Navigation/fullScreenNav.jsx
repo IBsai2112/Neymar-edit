45025de9941913ea44df891c695cfa989666268a
Sainjal Kalnekar <sainjalkalnekar21@gmail.com>
2026-02-04 22:50:39 +0530

    Fixing the Navbar bug

--- src/components/Navigation/fullScreenNav.jsx
+++ src/components/Navigation/fullScreenNav.jsx
@@ -4,34 +4,74 @@ const fullScreenNav = () => {
   return (
     <div id='fullScreenNav' className='bg-amber-500 text-white py-40 absolute w-full h-screen'>
         <div id='all-links'>
-            <div className='link border-t-1 border-white'>
+            <div className='link relative border-t-1 border-white'>
                 <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Projects</h1>
-                <div className='relative flex '>
-                    <div className='flex overflow-auto items-center '>
-                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian football legend</h2>
-                    <img className='h-2 w-6 ' src="../4-ney.jpeg" alt="" />
-                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian football legend</h2>
-                    <img className='h-2 w-6' src="../assets/14Ney.jpeg" alt="" />
+                <div className='moveLink absolute flex top-0 bg-[#fddf0b] text-black'>
+                    <div className='movex flex items-center '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../4-ney.jpeg" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>football legend</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../assets/19Ney.jpeg" alt="" />
                 </div>
+                    <div className='movex flex items-center '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../4-ney.jpeg" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>football legend</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../assets/19Ney.jpeg" alt="" />
+                </div>movex 
                 </div>
             </div>
-            {/* <div className='link border-t-1 border-white'>
-                <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Agence</h1>
-                <div>
-                    <div>
-                    <h2>Brazilian football legend</h2>
-                    <img src="../4-ney.jpeg" alt="" />
-                    <h2>Brazilian football legend</h2>
-                    <img src="../assets/14Ney.jpeg" alt="" />
+            <div className='link relative border-t-1 border-white'>
+                <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Projects</h1>
+                <div className='moveLink absolute flex top-0 bg-[#fddf0b] text-black'>
+                    <div className='movex flex items-center '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../4-ney.jpeg" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>football legend</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../assets/19Ney.jpeg" alt="" />
                 </div>
-                <div>
-                    <h2>Brazilian football legend</h2>
-                    <img src="../4-ney.jpeg" alt="" />
-                    <h2>Brazilian football legend</h2>
-                    <img src="../assets/14Ney.jpeg" alt="" />
+                    <div className='movex flex items-center '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../4-ney.jpeg" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>football legend</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../assets/19Ney.jpeg" alt="" />
+                </div>movex 
                 </div>
+            </div>
+            <div className='link relative border-t-1 border-white'>
+                <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Projects</h1>
+                <div className='moveLink absolute flex top-0 bg-[#fddf0b] text-black'>
+                    <div className='movex flex items-center '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../4-ney.jpeg" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>football legend</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../assets/19Ney.jpeg" alt="" />
                 </div>
-            </div> */}
+                    <div className='movex flex items-center '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../4-ney.jpeg" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>football legend</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../assets/19Ney.jpeg" alt="" />
+                </div>movex 
+                </div>
+            </div>
+            <div className='link relative border-t-1 border-white'>
+                <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Projects</h1>
+                <div className='moveLink absolute flex top-0 bg-[#fddf0b] text-black'>
+                    <div className='movex flex items-center '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../4-ney.jpeg" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>football legend</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../assets/19Ney.jpeg" alt="" />
+                </div>
+                    <div className='movex flex items-center '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>Brazilian</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../4-ney.jpeg" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-5.5 uppercase'>football legend</h2>
+                    <img className='h-23 w-62 shrink-0 object-cover' src="../assets/19Ney.jpeg" alt="" />
+                </div>movex 
+                </div>
+            </div>
         </div>
     </div>
   )
